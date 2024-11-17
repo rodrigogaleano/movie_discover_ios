56007bec4e398cac3f6e91abb35ccbb633b95192
Rodrigo Galeano <rodrigo1galeano@gmail.com>
2024-11-17 00:25:56 -0300

    Initial Commit

---
+++ MovieDiscover/MovieDiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDiscoverApp.swift
+//  MovieDiscover
+//
+//  Created by Rodrigo Galeano on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
